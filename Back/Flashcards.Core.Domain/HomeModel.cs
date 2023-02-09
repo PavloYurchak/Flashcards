ce7a5180d56c7494f422fb555e054bdcf410fb89
PavloYurchak <46574568+PavloYurchak@users.noreply.github.com>
2023-02-09 21:44:50 +0200

    init back-end

---
+++ Back/Flashcards.Core.Domain/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Flashcards.Core.Domain
+{
+    public class HomeModel
+    {
+        public HomeModel(string home)
+        {
+            Home= home;
+        }
+        public string? Home { get; set; }
+
+        public override string ToString()
+        {
+            return Home!;
+        }
+    }
+}
---
+++ Back/Flashcards.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Flashcards.Core.Domain;
+using Flashcards.Infrastructure.Persistence.Services.Home;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flashcards.WebApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHomeService _homeService;
+        public HomeController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetHome()
+        {
+            return Ok(await Task.FromResult(new HomeModel($"Controller <- {_homeService.GetHome()}")));
+        }
+    }
+}
